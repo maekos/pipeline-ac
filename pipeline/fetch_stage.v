2f19c80124414efeed0920f21631b50643295899
maekos@gmail.com <maekos@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2013-06-05 03:45:26 +0000

    [Codigo] Test a mux, modulo sumador y test de modulo sumador.

--- pipeline/fetch_stage.v
+++ pipeline/fetch_stage.v
@@ -1,39 +1,34 @@
 `timescale 1ns / 1ps
 //////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
 // Create Date:    19:32:42 06/04/2013 
-// Design Name: 
 // Module Name:    fetch_stage 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
+// Project Name:   pipeline 
+// Additional Comments: Etapa de busqueda de instruccion en memoria
+//								Tiene de salida la propia instruccion
 //////////////////////////////////////////////////////////////////////////////////
 module fetch_stage(
+	 input wire clk,
     input [6:0] pc_mux,
     output wire [6:0] pc_out,
     output wire [31:0] DR
     );
-	 
+	 // Declaracion de seniales internas
 	 wire [6:0] wire_pc;
 	 wire [6:0] wire_inc;
+	 wire [6:0] wire_mux;
 	 reg [6:0] PC;
 	 
 	 
 	 mem instruction_mem (
-		.clka(clka), 
-		.addra(addra), 
+		.clka(clk), 
+		.addra(wire_pc), 
 		.douta(DR)
 	);
 
+	// Cuerpo
+	always@(clk)
+	begin
+		PC = pc_out;
+	end
 	
 endmodule
