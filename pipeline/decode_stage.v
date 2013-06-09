64da1674c0e667879c3346198c4bc75ba21d5f7e
maekos@gmail.com <maekos@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2013-06-09 04:30:14 +0000

    [Codigo] Issue #9. Cambie tipo_instruccion por ctrl_unit.

--- pipeline/decode_stage.v
+++ pipeline/decode_stage.v
@@ -1,23 +1,10 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    20:41:51 06/06/2013 
-// Design Name: 
+////////////////////////////////////////////////
 // Module Name:    decode_stage 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+// Description: 	Etapa de decodificacion de
+//						de la instruccion que proviene
+//						desde la etapa fetch.
+////////////////////////////////////////////////
 module decode_stage(
     input [31:0] DR,
     input [31:0] WB,
@@ -25,21 +12,4 @@ module decode_stage(
     output [4:0] bus_B,
     output [31:0] ext_sig
     ); 
-	 
-	 wire load_store, alu_inm,branch, jump_abs, alu_reg, jump_rel, shift_var, shift;
-	 
-	tipo_instruccion tipo (
-		.DR(DR), 
-		.load_store(load_store), 
-		.alu_inm(alu_inm), 
-		.branch(branch), 
-		.jump_abs(jump_abs), 
-		.alu_reg(alu_reg), 
-		.jump_rel(jump_rel), 
-		.shift_var(shift_var), 
-		.shift(shift)
-	);
-	
-	
-
 endmodule
