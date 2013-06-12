1a12d932a0fd47c0ff19f65407c49697e62cf0b1
diario292@gmail.com <diario292@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2013-06-12 00:11:55 +0000

    [Codigo] Issue #7 Unidad de control sin jump.

--- trunk/pipeline/ctrl_unit_test.v
+++ trunk/pipeline/ctrl_unit_test.v
@@ -36,7 +36,7 @@ module ctrl_unit_test;
 	wire ALUSrc;
 	wire RegWrite;
 	wire RegDst;
-	wire [1:0] AluOp;
+	wire [4:0] AluOp;
 
 	// Instantiate the Unit Under Test (UUT)
 	ctrl_unit uut (
@@ -59,15 +59,46 @@ module ctrl_unit_test;
 		#100;
 		opcode = 6'b100000;
 		#100;
+		opcode = 6'b100001;
+		#100;
+		opcode = 6'b100011;
+		#100;
+		opcode = 6'b100111;
+		#100;
+		opcode = 6'b100100;
+		#100;
+		opcode = 6'b100101;
+		#100;
 		opcode = 6'b101000;
 		#100;
+		opcode = 6'b101001;
+		#100;
+		opcode = 6'b101011;
+		#100;
+		opcode = 6'b001000;
+		#100;
+		opcode = 6'b001001;
+		#100;
+		opcode = 6'b001100;
+		#100;
+		opcode = 6'b001101;
+		#100;
+		opcode = 6'b001110;
+		#100;
+		opcode = 6'b001111;
+		#100;
+		opcode = 6'b001010;
+		#100;
+		opcode = 6'b001011;
+		#100;
 		opcode = 6'b000100;
+		#100;
+		opcode = 6'b000101;
 		#100;
 		// Wait 100 ns for global reset to finish
 		#100;
         
 		// Add stimulus here
-
 	end
       
 endmodule
