c7c71a66ed2e93f17153c24120b29830da5c4afc
diario292@gmail.com <diario292@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2013-06-24 22:55:56 +0000

    [Codigo] Issue #7. Unidad de control terminada.

--- trunk/pipeline/ctrl_unit.v
+++ trunk/pipeline/ctrl_unit.v
@@ -6,16 +6,17 @@
 //////////////////////////////////////////
 module ctrl_unit(
     input [5:0] opcode,
-	 output jump,
-	 output jal,
-	 output branch,
+	 output Jump,
+	 output Jal,
+	 output Branch,
 	 output MemRead,
-	 output MemtoReg,
 	 output MemWrite,
-	 output ALUSrc,
-	 output RegWrite,
+	 output MemtoReg,
+	 output RegSrc,
 	 output RegDst,
-	 output jal_signal,
+	 output RegWrite,
+	 output PCSrc,
+	 output ALUSrc,
 	 output [4:0] AluOp
 	);
 	
@@ -26,19 +27,20 @@ module ctrl_unit(
 	assign r_type	= (~opcode[0])&&(~opcode[1])&&(~opcode[2])&&(~opcode[3])&&(~opcode[4])&&(~opcode[5]);
 	assign j 		= (~opcode[0])&&(opcode[1])&&(~opcode[2])&&(~opcode[3])&&(~opcode[4])&&(~opcode[5]);
 	assign jal		= (opcode[0])&&(opcode[1])&&(~opcode[2])&&(~opcode[3])&&(~opcode[4])&&(~opcode[5]);
-	assign jr		= (~opcode[0])&&(~opcode[1])&&(~opcode[2])&&(opcode[3])&&(~opcode[4])&&(~opcode[5]);
-	assign jalr		= (opcode[0])&&(~opcode[1])&&(~opcode[2])&&(opcode[3])&&(~opcode[4])&&(~opcode[5]);
+	assign jr		= (opcode[0])&&(opcode[1])&&(opcode[2])&&(~opcode[3])&&(~opcode[4])&&(~opcode[5]);
+	assign jalr		= (~opcode[0])&&(~opcode[1])&&(opcode[2])&&(~opcode[3])&&(opcode[4])&&(~opcode[5]);
 	
-	assign RegDst 		= (~load) && (r_type || jalr) && (~i_type);
-	assign ALUSrc 		= (load || store || i_type) && (~r_type) && (~b_type);
-	assign MemtoReg 	= load && (~r_type) && (~i_type);
-	assign RegWrite 	= (load || r_type || i_type || jal || jalr) &&(~store) && (~b_type);
-	assign MemRead 	= load && (~store) && (~r_type) && (~b_type);
-	assign MemWrite 	= (~load) && store && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jal) && (~jalr) && (~jr);
-	assign branch 		= (~load) && (~store) && (~r_type) && b_type && (~i_type) && (~j) && (~jal) && (~jalr) && (~jr);
-	assign jump			= (j || jal) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~jalr) && (~jr);
-	assign RegSrc		= (jal || jalr) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jr);
-	assign PCSrc		= (jr || jalr) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jal);
+	assign Jump			= (j || jal) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~jalr) && (~jr);		//Instruccion que realiza un salto
+	assign Jal			= jal;
+	assign Branch 		= (~load) && (~store) && (~r_type) && b_type && (~i_type) && (~j) && (~jal) && (~jalr) && (~jr);	//Instruccion que realiza un branch
+	assign MemRead 	= load && (~store) && (~r_type) && (~b_type);																		//La instruccion requiere una lectura de memoria
+	assign MemWrite 	= (~load) && store && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jal) && (~jalr) && (~jr);	//La instruccion desencadena una escritura en memoria
+	assign MemtoReg 	= load && (~r_type) && (~i_type);																						//La instruccion guarda un dato de memoria en un registro
+	assign RegSrc		= (jal || jalr) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jr);		//Dato para operar proveniente de un registro
+	assign RegDst 		= (~load) && (r_type || jalr) && (~i_type);																			//Un dato se guardara en el registro
+	assign RegWrite 	= (load || r_type || i_type || jal || jalr) &&(~store) && (~b_type);											//Se escribira en un registro
+	assign PCSrc		= (jr || jalr) && (~load) && (~store) && (~r_type) && (~b_type) && (~i_type) && (~j) && (~jal);		//Multiplexor de 
+	assign ALUSrc 		= (load || store || i_type) && (~r_type) && (~b_type);															//Multiplexor de segunda entrada de la ALU
 	assign AluOp[0] 	= (~load) && (~store) && (~r_type) && (b_type || i_type);
 	assign AluOp[1] 	= (~load) && (~store) && (r_type || i_type) && (~b_type);
 	assign AluOp[2]	= opcode[0];
