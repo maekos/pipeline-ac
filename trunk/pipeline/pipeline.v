3fbec66eb008e7bf3181e5e34a518951af7b602a
diario292@gmail.com <diario292@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2014-03-20 20:01:40 +0000

    [Code] Arreglados los warning, falta probar en placa.

--- trunk/pipeline/pipeline.v
+++ trunk/pipeline/pipeline.v
@@ -1,28 +1,9 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    23/03/2014 
-// Design Name: 
-// Module Name:    pipeline 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
 
 module pipeline(
-		input clk,
-		input rst,
-		input enable,
+		input wire clk,
+		input wire rst,
+		input wire enable,
 		output wire [31:0] instruccion, 
 		output wire [6:0] pc,
 		output wire [38:0] if_id, //[38:32] next_pc_if_id, [31:0] instruction_if_id
@@ -30,24 +11,30 @@ module pipeline(
 		output wire [71:0] ex_m, //mem_to_reg_ex_m,reg_write_ex_m,mem_write_ex_m,[31:0] alu_result_ex_m,[31:0] data2_ex_m,[4:0] dst_ex_m,
 		output wire [70:0] m_wb, //reg_write_m_wb,mem_to_reg_m_wb,[31:0] alu_result_m_wb,[31:0] data_load_m_wb,[4:0] dst_m_wb,
 		output wire [1023:0] registros
-    );
+		);
 	 
 	 wire stop;
 	 wire [6:0] pc_out;
-	 wire pc_src;
-	 wire [6:0] pc_branch_fetch ;
+	 wire [6:0] branch_pc ;
 	 wire [31:0] DR;
 	 wire stop_enable;
+	 wire [31:0] data1;
+	 wire [31:0] data2;
+	 wire [31:0] ext_sig;
+	 wire [11:0] palabra;
+	 wire equals;
+	 assign equals = ((data1 ~^ data2) & palabra[11]); // Para chequear que los datos son iguales en la salida del banco de registros.
+	
 	 
 	 assign stop_enable = enable & (~stop);
 	 
 	 /* Bloque de busqueda de instruccion */
 	fetch_stage ifetch_instance (
 		.clk(~clk), // conectado
-		.dec(pc_src), // conectado
+		.dec(equals), // conectado
 		.rst(rst), 
 		.enbl(stop_enable),
-		.pc_mux(pc_branch_fetch), // conectado
+		.pc_mux(branch_pc), // conectado
 		.pc_out(pc_out),  // conectado
 		.DR(DR) // conectado
 	); 
@@ -74,16 +61,7 @@ module pipeline(
 	 wire reg_write_in;
 	 wire [4:0] WR;
 	 wire [31:0] WD;
-	 wire [31:0] data1;
-	 wire [31:0] data2;
-	 wire [31:0] ext_sig;
-	 wire branch;
-	 wire mem_write;
-	 wire mem_to_reg;
-	 wire reg_dst;
-	 wire reg_write_out;
-	 wire alu_src;
-	 wire [5:0] alu_op;
+	 
 	 
 	 decode_stage decode_instance (
 		.clk(~clk), 
@@ -97,15 +75,16 @@ module pipeline(
 		.data1(data1), // Conectado
 		.data2(data2), // Conectado
 		.ext_sig(ext_sig), // Conectado
-		.branch(branch), // Conectado
-		.mem_write(mem_write), // Conectado
-		.mem_to_reg(mem_to_reg), // Conectado
-		.reg_dst(reg_dst), // Conectado
-		.reg_write_out(reg_write_out), // Conectado
-		.alu_src(alu_src), // Conectado
-		.alu_op(alu_op),// Conectado
+		.palabra_salida(palabra), //  VERIFICAR!!
 		.registros(registros)
 	);
+	
+	branch_sum next_hop (
+		.rst(rst),
+		.pc_next(next_pc_reg), 
+		.pc_branch(ext_sig[6:0]), 
+		.branch_pc(branch_pc)
+	 );
 	
 	/* Latch entre idecode e iexecute */
 	 wire [4:0] rs;
@@ -113,26 +92,22 @@ module pipeline(
 	 wire [31:0] data1_ex;
 	 wire [31:0] data2_ex;
 	 wire [31:0] sign_extend_ex;
-	 wire branch_ex;
 	 wire mem_write_ex;
 	 wire mem_to_reg_ex;
 	 wire reg_dst_ex;
 	 wire reg_write_ex;
 	 wire alu_src_ex;
 	 wire [5:0] alu_op_ex;
-	 wire [6:0] pc_next_ex;
 	 wire [4:0] reg1_ex;
 	 wire [4:0] reg2_ex;
-	
+	 
 	latch_id_ex id_ex_instance (
-		.alu_op(alu_op), // Conectado
-		.reg_dst(reg_dst), // Conectado
-		.alu_src(alu_src), // Conectado
-		.branch(branch), // Conectado
-		.mem_write(mem_write), // Conectado
-		.reg_write(reg_write_out), // Conectado 
-		.mem_to_reg(mem_to_reg), // Conectado
-		.pc_next(next_pc_reg), // conectado 
+		.alu_op(palabra[5:0]), // Conectado
+		.reg_dst(palabra[8]), // Conectado
+		.alu_src(palabra[6]), // Conectado
+		.mem_write(palabra[10]), // Conectado
+		.reg_write(palabra[7]), // Conectado 
+		.mem_to_reg(palabra[9]), // Conectado
 		.data1(data1), // Conectado
 		.data2(data2), // Conectado
 		.sign_extend(ext_sig), // Conectado 
@@ -144,12 +119,10 @@ module pipeline(
 		.rst(rst),
 		.alu_op_reg(alu_op_ex), //Conectado 
 		.reg_dst_reg(reg_dst_ex), //Conectado
-		.alu_src_reg(alu_src_ex), //Conectado 
-		.branch_reg(branch_ex), //Conectado
+		.alu_src_reg(alu_src_ex), //Conectado
 		.mem_write_reg(mem_write_ex), //Conectado
 		.reg_write_reg(reg_write_ex), //Conectado
 		.mem_to_reg_reg(mem_to_reg_ex), //Conectado
-		.pc_next_reg(pc_next_ex), //Conectado
 		.data1_reg(data1_ex), //Conectado
 		.data2_reg(data2_ex), //Conectado
 		.sign_extend_reg(sign_extend_ex), //Conectado 
@@ -165,8 +138,6 @@ module pipeline(
 	wire [31:0] cortoB_out;
 	
 	/* bloque de etapa execute */
-	wire [6:0] branch_pc;
-	wire zero;
 	wire [31:0] alu_result;
 	wire [31:0] data2_out;
 	wire [4:0] dst;
@@ -176,14 +147,11 @@ module pipeline(
 		.alu_src(alu_src_ex), //Conectado
 		.alu_op(alu_op_ex), //Conectado
 		.reg_dst(reg_dst_ex), //Conectado 
-		.pc_next(pc_next_ex), //Conectado
 		.data1(cortoA_out), //Conectado 
 		.data2(cortoB_out), //Conectado
 		.sign_extend(sign_extend_ex), //Conectado 
 		.reg1(reg1_ex), //Conectado 
 		.reg2(reg2_ex), //Conectado
-		.branch_pc(branch_pc), //Conectado 
-		.zero(zero), //Conectado
 		.alu_result(alu_result), //Conectado
 		.data2_out(data2_out), //Conectado
 		.dst(dst) //Conectado
@@ -193,9 +161,7 @@ module pipeline(
 	/* Latch etapa execute memory */
 	wire mem_to_reg_m; 
    wire reg_write_m; 
-	wire branch_m; 
 	wire mem_write_m; 
-	wire zero_m; 
 	wire [31:0] alu_result_m; 
 	wire [31:0] data2_m; 
 	wire [4:0] dst_m;
@@ -203,10 +169,7 @@ module pipeline(
 	latch_ex_m ex_m_instance (
 		.mem_to_reg(mem_to_reg_ex), //Conectado
 		.reg_write(reg_write_ex), //Conectado
-		.branch(branch_ex), //Conectado
 		.mem_write(mem_write_ex), //Conectado
-		.pc_branch(branch_pc), // conectado
-		.zero(zero), //Conectado
 		.alu_result(alu_result), //Conectado 
 		.data2(data2_out), //Conectado 
 		.dst(dst), //Conectado
@@ -214,10 +177,7 @@ module pipeline(
 		.rst(rst),
 		.mem_to_reg_reg(mem_to_reg_m), //Conectado
 		.reg_write_reg(reg_write_m), //Conectado
-		.branch_reg(branch_m), //Conectado
 		.mem_write_reg(mem_write_m), //Conectado
-		.pc_branch_reg(pc_branch_fetch), //Conectado
-		.zero_reg(zero_m), //Conectado
 		.alu_result_reg(alu_result_m), //Conectado
 		.data2_reg(data2_m), //Conectado
 		.dst_reg(dst_m) //Conectado
@@ -227,14 +187,11 @@ module pipeline(
 	wire [31:0] data_out;
 	
 	mem_stage mem_instance (
-		.no_clk(~clk), //Conectado
-		.branch(branch_m), //Conectado
-		.zero(zero_m), //Conectado  
+		.no_clk(~clk), //Conectado 
 		.mem_write(mem_write_m), //Conectado
 		.address(alu_result_m[4:0]), //Conectado 
 		.write_data(data2_m), //Conectado 
-		.data_out(data_out), //Conectado
-		.pc_src(pc_src) //Conectado
+		.data_out(data_out) //Conectado
 	);
 	
 	/* Latch imem e iwb */
@@ -302,14 +259,12 @@ module pipeline(
 
 	//_-_-_-_-_-_-_-_-Unidad de Detección de Riesgos de Datos-_-_-_-_-_-_-_-_-_//
 
-	hazard data_hazard (
+	hazard data_hazard_instance (
 		.instruction(instruction_reg[25:16]), 
 		.rt_ex(rt), 
 		.mem_to_reg_ex(mem_to_reg_ex), 
 		.stop(stop)
 	);
-	
-	//_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_-_//
 	
 	assign instruccion = DR; 
 	assign pc = pc_out;
