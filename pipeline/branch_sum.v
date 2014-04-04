fcaea1d6a03298555efe23bab4b34a69a8a9cd6b
diario292@gmail.com <diario292@gmail.com@c58119b4-5164-2421-d6c0-d69afe885e01>
2014-04-04 03:04:12 +0000

    [Code] Warning Route:452 generando problemas. Quitar.

--- pipeline/branch_sum.v
+++ pipeline/branch_sum.v
@@ -1,33 +1,57 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date:    19:24:35 12/02/2013 
-// Design Name: 
-// Module Name:    branch_sum 
-// Project Name: 
-// Target Devices: 
-// Tool versions: 
-// Description: 
-//
-// Dependencies: 
-//
-// Revision: 
-// Revision 0.01 - File Created
-// Additional Comments: 
-//
-//////////////////////////////////////////////////////////////////////////////////
+
 module branch_sum  #(parameter PC_WIDE = 7)(
     input rst,
+	 input [5:0]opcode,
+	 input [31:0] d1,
+	 input [31:0] d2,
 	 input [PC_WIDE - 1:0] pc_next,
     input [6:0] pc_branch,
+	 output reg taken,
     output reg [PC_WIDE - 1:0] branch_pc
     );
 	 
-	 always @(pc_next or pc_branch or rst) begin
-			if (rst == 1) branch_pc <= 0;
-			else branch_pc <= pc_next + pc_branch[PC_WIDE - 1:0];
-	 end
+	parameter BEQ = 6'b000100;
+	parameter BNE = 6'b000101;
+	parameter JUMP = 6'b000010;
+	
+	always @(*) begin
+		if (rst == 1) begin
+			branch_pc <= 0;
+			taken <= 0;
+		end
+		else begin
+			case(opcode)
+				BEQ: begin
+							if(d1 == d2) begin
+								branch_pc <= pc_next + pc_branch[PC_WIDE - 1:0];
+								taken <= 1;
+							end
+							else begin
+								taken <= 0;
+								branch_pc <= 0;
+							end
+						end
+				BNE: begin
+							if(d1 != d2) begin
+								branch_pc <= pc_next + pc_branch[PC_WIDE - 1:0];
+								taken <= 1;
+							end
+							else begin
+								taken <= 0;
+								branch_pc <= 0;
+							end
+						end
+				JUMP: begin
+							branch_pc <= pc_branch;
+							taken <= 1;
+						end
+				default: begin
+								branch_pc <= 0;
+								taken <= 0;
+						end
+			endcase
+		end
+	end
 	
 endmodule
